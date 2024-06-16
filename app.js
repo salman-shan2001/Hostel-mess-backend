80f8993dc6450a3c28447c89f9ffe76f454d01a0
salman-shan2001 <salmanfarizy454@gmail.com>
2024-06-16 19:51:57 +0530

    updated successfuly

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const { MessModel } = require("./modules/mess")     // messmodule imported
+const bcryptjs=require("bcryptjs")
 
 //mongoose database link
 mongoose.connect("mongodb+srv://salmanshan:salman642001@cluster0.odxej1b.mongodb.net/StudentMessDB?retryWrites=true&w=majority&appName=Cluster0")
@@ -14,13 +15,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+//creating hash function for generate hashed password
+const GenerateHashedPassword = async (Password) => {
+const salt=await bcryptjs.genSalt(10)
+return bcryptjs.hash(Password,salt)
+}
+
 // creating SignUp API
 
-app.post("/SignUp", (req, res) => {
+app.post("/SignUp", async(req, res) => {
     let input = req.body
     // console.log(input)
+    let HashedPassword= await GenerateHashedPassword(input.Password)
+    console.log(HashedPassword)
+    input.Password=HashedPassword
     const Mess = new MessModel(input)
-    console.log(Mess)
+    //console.log(Mess)
     Mess.save()
     res.json({ "status": "success" })
 })
