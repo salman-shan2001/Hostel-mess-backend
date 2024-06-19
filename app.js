f7b82b3bc6f1871115cc4a597392fadbae7805ab
firdhousfiru <firdhouskh2002@gmail.com>
2024-06-19 22:46:58 +0530

    signin

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const { MessModel } = require("./modules/mess")     // messmodule imported
-const bcryptjs=require("bcryptjs")
+const bcryptjs = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const { StaffModel } = require("./modules/staff")
 
@@ -19,52 +19,52 @@ app.use(express.json())
 
 //creating hash function for generate hashed password
 const GenerateHashedPassword = async (Password) => {
-const salt=await bcryptjs.genSalt(10)
-return bcryptjs.hash(Password,salt)
+    const salt = await bcryptjs.genSalt(10)
+    return bcryptjs.hash(Password, salt)
 }
 
 // creating SignUp API
 
-app.post("/SignUp", async(req, res) => {
+app.post("/SignUp", async (req, res) => {
     let input = req.body
     // console.log(input)
-    let HashedPassword= await GenerateHashedPassword(input.Password)
+    let HashedPassword = await GenerateHashedPassword(input.Password)
     console.log(HashedPassword)
-    input.Password=HashedPassword
+    input.Password = HashedPassword
     const Mess = new MessModel(input)
     //console.log(Mess)
     Mess.save()
     res.json({ "status": "success" })
 })
 
-app.post("/SignIn",(req,res)=>{
-    let input=req.body
-    MessModel.find({"EmailId":req.body.EmailId}).then(
-        (response)=>{
-           if (response.length>0) {
-            let dbPassword=response[0].Password
-            console.log(dbPassword)
-            bcryptjs.compare(input.Password,dbPassword,(error,isMatch)=>{
-                if (isMatch) {
-                    jwt.sign({ EmailId: input.EmailId }, "mess-app", { expiresIn: "1d" },
-                        (error, token) => {
-                            if (error) {
-                               res.json({ "status": "unable to create token" })
-                            } else {
-                                res.json({ "status": "success", "userid": response[0]._id, "token": token })
+app.post("/SignIn", (req, res) => {
+    let input = req.body
+    MessModel.find({ "EmailId": req.body.EmailId }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbPassword = response[0].Password
+                console.log(dbPassword)
+                bcryptjs.compare(input.Password, dbPassword, (error, isMatch) => {
+                    if (isMatch) {
+                        jwt.sign({ EmailId: input.EmailId }, "mess-app", { expiresIn: "1d" },
+                            (error, token) => {
+                                if (error) {
+                                    res.json({ "status": "unable to create token" })
+                                } else {
+                                    res.json({ "status": "success", "userid": response[0]._id, "token": token })
+                                }
                             }
-                        }
-                    )
-                } else {
-                    res.json({"status":"incorrect password"})
-                    
-                }
-            })
+                        )
+                    } else {
+                        res.json({ "status": "incorrect password" })
 
-           } else {
-            res.json({satus:"user not found"})
-            
-           }
+                    }
+                })
+
+            } else {
+                res.json({ "status": "user not found" })
+
+            }
         }
     ).catch()
 
@@ -91,16 +91,50 @@ app.post("/viewStudent",(req,res)=>{
 })
 
 //Staff Signup
-app.post("/StaffSignup",async(req,res)=>{
-   let input = req.body
-   let HashedPassword=await GenerateHashedPassword(input.StaffPassword)
+app.post("/StaffSignup", async (req, res) => {
+    let input = req.body
+    let HashedPassword = await GenerateHashedPassword(input.StaffPassword)
     console.log(HashedPassword)
-input.StaffPassword=HashedPassword
-const Staff=new StaffModel(input)
-Staff.save()
-    res.json({"status":"success"})
-    
+    input.StaffPassword = HashedPassword
+    const Staff = new StaffModel(input)
+    Staff.save()
+    res.json({ "status": "success" })
+
 })
+//StaffSignIn
+app.post("/StaffSignIn", (req, res) => {
+    let input = req.body
+    StaffModel.find({ "Staff EmailId": req.body.StaffEmailId }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbPassword = response[0].StaffPassword
+                console.log(dbPassword)
+                bcryptjs.compare(input.StaffPassword, dbPassword, (error, isMatch) => {
+                    if (isMatch) {
+                        jwt.sign({ StaffEmailId: input.StaffEmailId }, "mess-app", { expiresIn: "1d" },
+                            (error, token) => {
+                                if (error) {
+                                    res.json({ "status": "unable to create token" })
+                                } else {
+                                    res.json({ "status": "success", "userid": response[0]._id, "token": token })
+                                }
+                            }
+                        )
+                    } else {
+                        res.json({ "status": "incorrect password" })
+
+                    }
+                })
+
+            }
+            else {
+                res.json({ "status": "user ot found" })
+            }
+        }
+    ).catch()
+    //res.json({"status":"success"})
+})
+
 
 
 //to view the server updates
